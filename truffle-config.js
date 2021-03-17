3f8cff190fbd4bae0e35624f5a3e2e386c080e4a
Tamil selvi <tamilselvivenky99@gmail.com>
2021-03-17 16:03:16 +0530

    Azure deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
+  contracts_build_directory: "./Web-client/src/contracts",
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
@@ -6,6 +8,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    azure: {
+      provider: new HDWalletProvider("fiber bread journey cook vendor also half check unhappy room give dynamic","http://ethqfie4f-dns-reg1.eastus.cloudapp.azure.com:8540"),
+      network_id: "*",
+      gasPrice:0
     },
     develop: {
       port: 8545
